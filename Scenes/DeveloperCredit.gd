edbaa88aff0893f6c93758f19eb81179f6142afe
Prakhar Sharma <prakhar29sharma@gmail.com>
2024-02-03 18:56:53 +0530

    developer credit scene

---
+++ Scenes/DeveloperCredit.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://.godot/exported/133200997/export-53a0af8f00fbc899d4d541c34a803049-menu.scn")
